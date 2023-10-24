df738f3b3d23553069905b35c4ae47a0d37fb8bb
saucyy <131738356+Berrytree5@users.noreply.github.com>
2023-10-23 22:43:44 -0700

    added return codes for invalid entries

--- app.js
+++ app.js
@@ -47,21 +47,42 @@ function mainMenu() {
           console.log('Exiting the application.');
           process.exit();
           break;
+        default:
+          console.log('Invalid option. Please select a valid action.');
+          mainMenu(); // Show the menu again
+          break;
       }
-      mainMenu();
     });
 }
 
 async function viewAllDepartments() {
-
+  try {
+    const departments = await EmployeeQueries.getAllDepartments();
+    console.table(departments);
+  } catch (err) {
+    console.error('Error fetching departments:', err);
+  }
+  mainMenu(); // Show the menu again
 }
 
 async function viewAllRoles() {
-  
+  try {
+    const roles = await EmployeeQueries.getAllRoles();
+    console.table(roles);
+  } catch (err) {
+    console.error('Error fetching roles:', err);
+  }
+  mainMenu(); // Show the menu again
 }
 
 async function viewAllEmployees() {
-  
+  try {
+    const employees = await EmployeeQueries.getAllEmployees();
+    console.table(employees);
+  } catch (err) {
+    console.error('Error fetching employees:', err);
+  }
+  mainMenu(); // Show the menu again
 }
 
 async function addDepartment() {
@@ -71,23 +92,159 @@ async function addDepartment() {
         type: 'input',
         name: 'name',
         message: 'Enter the name of the department:',
+        validate: (input) => {
+          if (input.trim() === '') {
+            return 'Department name cannot be empty.';
+          }
+          return true;
+        },
       },
     ])
     .then(async (answers) => {
-    
+      try {
+        const existingDepartments = await EmployeeQueries.getAllDepartments();
+        const departmentExists = existingDepartments.some((department) => department.name === answers.name);
+
+        if (departmentExists) {
+          console.log('Department already exists.');
+        } else {
+          await EmployeeQueries.addDepartment(answers.name);
+          console.log(`Department "${answers.name}" added successfully.`);
+        }
+      } catch (err) {
+        console.error('Error adding department:', err);
+      }
+      mainMenu(); // Show the menu again
     });
 }
 
 async function addRole() {
-  // User fills out role information
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the title of the role:',
+        validate: (input) => {
+          if (input.trim() === '') {
+            return 'Role title cannot be empty.';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: 'Enter the salary for the role:',
+        validate: (input) => {
+          if (isNaN(input) || parseFloat(input) <= 0) {
+            return 'Invalid salary. Please enter a positive number.';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'departmentId',
+        message: 'Enter the department ID for the role:',
+        validate: async (input) => {
+          const departments = await EmployeeQueries.getAllDepartments();
+          const departmentIds = departments.map((department) => department.id);
+
+          if (!departmentIds.includes(parseInt(input))) {
+            return 'Invalid department ID. Please enter a valid department ID.';
+          }
+          return true;
+        },
+      },
+    ])
+    .then(async (answers) => {
+      try {
+        await EmployeeQueries.addRole(answers.title, parseFloat(answers.salary), parseInt(answers.departmentId));
+        console.log(`Role "${answers.title}" added successfully.`);
+      } catch (err) {
+        console.error('Error adding role:', err);
+      }
+      mainMenu(); // Show the menu again
+    });
 }
 
 async function addEmployee() {
-  // User fills out Add Employee prompt
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'firstName',
+        message: "Enter the employee's first name:",
+        validate: (input) => {
+          if (input.trim() === '') {
+            return "First name cannot be empty.";
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'lastName',
+        message: "Enter the employee's last name:",
+        validate: (input) => {
+          if (input.trim() === '') {
+            return "Last name cannot be empty.";
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'roleId',
+        message: 'Enter the role ID for the employee:',
+        validate: async (input) => {
+          const roles = await EmployeeQueries.getAllRoles();
+          const roleIds = roles.map((role) => role.id);
+
+          if (!roleIds.includes(parseInt(input))) {
+            return 'Invalid role ID. Please enter a valid role ID.';
+          }
+          return true;
+        },
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: "Enter the manager's ID (or leave blank if there's no manager):",
+        validate: async (input) => {
+          if (input.trim() === '') {
+            return true; // No manager, so no validation needed
+          }
+
+          const employees = await EmployeeQueries.getAllEmployees();
+          const employeeIds = employees.map((employee) => employee.id);
+
+          if (!employeeIds.includes(parseInt(input))) {
+            return 'Invalid manager ID. Please enter a valid manager ID or leave it blank.';
+          }
+          return true;
+        },
+      },
+    ])
+    .then(async (answers) => {
+      try {
+        await EmployeeQueries.addEmployee(
+          answers.firstName,
+          answers.lastName,
+          parseInt(answers.roleId),
+          answers.managerId ? parseInt(answers.managerId) : null
+        );
+        console.log(`Employee "${answers.firstName} ${answers.lastName}" added successfully.`);
+      } catch (err) {
+        console.error('Error adding employee:', err);
+      }
+      mainMenu(); // Show the menu again
+    });
 }
 
 async function updateEmployeeRole() {
-  // User updates employee role when prompted
+  // Implementation for updating an employee's role
 }
 
 mainMenu();
